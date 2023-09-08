c4e4353cee76c5b2809274d888553285d0172fbe
Livni <llmc0383@gmail.com>
2023-09-07 20:48:57 -0600

    se creo el controlador, la vista del mapa y se agregaron rutas al layout

--- controllers/MapaController.php
+++ controllers/MapaController.php
@@ -2,10 +2,46 @@
 
 namespace Controllers;
 
+use Model\Marcador;
 use MVC\Router;
 
 class MapaController{
     public static function index(Router $router){
         $router->render('mapa/index', []);
     }
+
+    public static function guardarApi(){
+     
+        try {
+            $marcador = new Marcador($_POST);
+            $resultado = $marcador->crear();
+
+            if ($resultado['resultado'] == 1) {
+                echo json_encode([
+                    'mensaje' => 'Registro guardado correctamente',
+                    'codigo' => 1
+                ]);
+            } else {
+                echo json_encode([
+                    'mensaje' => 'Ocurrió un error',
+                    'codigo' => 0
+                ]);
+            }
+
+            // echo json_encode($resultado);
+            
+        } catch (Exception $e) {
+            echo json_encode([
+                'detalle' => $e->getMessage(),
+                'mensaje' => 'Ocurrió un error',
+                'codigo' => 0
+            ]);
+        }
+    }
+
 }
+ 
+?>
+
+
+
